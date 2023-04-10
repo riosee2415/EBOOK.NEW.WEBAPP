00a7472b802a4b6cd61ac82269a805426d9cb2ef
hmg11123 <4leaf.hmg@gmail.com>
2023-04-10 10:20:16 +0900

    modify

--- back/routers/bannerRouter.js
+++ back/routers/bannerRouter.js
@@ -38,7 +38,7 @@ const upload = multer({
       );
     },
   }),
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  limits: { fileSize: 50 * 1024 * 1024 }, // 50MB
 });
 
 // const upload = multer({
